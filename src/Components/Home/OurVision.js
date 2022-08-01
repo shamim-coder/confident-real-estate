a1bfa6005ba6142ca333cd944383ac954c40883c
wpshamim <wpshamim@gmail.com>
2022-08-01 23:10:33 +0600

    update

--- src/Components/Home/OurVision.js
+++ src/Components/Home/OurVision.js
@@ -13,21 +13,21 @@ const OurVision = () => {
                         <h1 class="text-5xl md:text-7xl font-bold">
                             Our <span className="text-primary">Vision</span>
                         </h1>
-                        <p class="py-6 text-lg">We have prioritized the following key quality assurance goals</p>
+                        <p class="py-6 text-xl font-semibold">We have prioritized the following key quality assurance goals</p>
                         <ul className="mb-5">
-                            <li className="mb-4 font-semibold flex items-center gap-3">
+                            <li className="mb-4 flex items-center gap-3 text-lg">
                                 <FontAwesomeIcon className="text-primary mr-3" icon={faPlay} /> We aim to meet our Client Project needs at the given time and budget with highest quality possible.
                             </li>
-                            <li className="mb-4 font-semibold flex items-center gap-3">
+                            <li className="mb-4  flex items-center gap-3 text-lg">
                                 <FontAwesomeIcon className="text-primary mr-3" icon={faPlay} /> We want to increase our value to our customers so that they can be proud to be associated with our company.
                             </li>
-                            <li className="mb-4 font-semibold flex items-center gap-3">
+                            <li className="mb-4 flex items-center gap-3 text-lg">
                                 <FontAwesomeIcon className="text-primary mr-3" icon={faPlay} /> We want implement to a project with the best possible solutions to meet the needs of a customer.
                             </li>
-                            <li className="mb-4 font-semibold flex items-center gap-3">
+                            <li className="mb-4 flex items-center gap-3 text-lg">
                                 <FontAwesomeIcon className="text-primary mr-3" icon={faPlay} /> We are always considering the long term harm to the environment to the solutions we provide in a project.
                             </li>
-                            <li className="mb-4 font-semibold flex items-center gap-3">
+                            <li className="mb-4  flex items-center gap-3 text-lg">
                                 <FontAwesomeIcon className="text-primary mr-3" icon={faPlay} /> <div>We want to involve our clients, Suppliers and our partners to our project with a aim to promote good practice.</div>
                             </li>
                         </ul>
