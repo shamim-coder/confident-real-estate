4e725e4bc701cb52fd5240dad4cff784e6fbddcc
wpshamim <wpshamim@gmail.com>
2022-08-01 23:00:42 +0600

    add navigate

--- src/Components/Home/Banner.js
+++ src/Components/Home/Banner.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Home.css";
+import { useNavigate } from "react-router-dom";
 
 const Banner = () => {
+    const navigate = useNavigate();
     return (
         <div className="banner flex flex-col justify-center items-center text-center py-10 md:py-32">
             <div className="lg:w-1/2 text-base-100">
@@ -10,7 +12,9 @@ const Banner = () => {
                     Confident <span className="bg-primary px-5">Real Estate</span> & Construction
                 </h1>
                 <p className="text-lg">You can dream, create, design, and build the most wonderful place in the world. But it requires more experience people to make the dream a reality.</p>
-                <div className="btn btn-primary mt-10">Explore Services</div>
+                <button onClick={() => navigate("/services")} className="btn btn-primary mt-10">
+                    Explore Services
+                </button>
             </div>
         </div>
     );
