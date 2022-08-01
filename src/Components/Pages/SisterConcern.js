65035abebd4533bc6c40eb23e99654b3e7942477
wpshamim <wpshamim@gmail.com>
2022-08-01 22:58:16 +0600

    update

--- src/Components/Pages/SisterConcern.js
+++ src/Components/Pages/SisterConcern.js
@@ -1,14 +1,11 @@
 import React from "react";
-import akramTrading from "../../Assets/akram-trading.jpg";
-import mahiTimber from "../../Assets/mahi-timber.jpg";
-import confidetnAgro from "../../Assets/confident-agro.jpg";
 
 const SisterConcern = () => {
     return (
         <section class="py-20">
             <div class="hero mb-10">
                 <div class="hero-content flex-col lg:flex-row-reverse gap-10">
-                    <img src={akramTrading} class="max-w-lg w-full rounded-lg shadow-2xl" alt="" />
+                    <img src={"https://i.ibb.co/b5wmmfW/akram-trading.jpg"} class="max-w-lg w-full rounded-lg shadow-2xl" alt="" />
                     <div>
                         <h1 class="text-5xl font-bold">Akram Trading Corporation</h1>
                         <p class="py-6">According to the needs of our company or for the need of any buyer we import any types of Product or Machinery in a limited scale.</p>
@@ -25,13 +22,13 @@ const SisterConcern = () => {
                             accordingly.
                         </p>
                     </div>
-                    <img src={mahiTimber} class="max-w-lg w-full rounded-lg shadow-2xl" alt="" />
+                    <img src={"https://i.ibb.co/9HhWFBJ/mahi-timber.jpg"} class="max-w-lg w-full rounded-lg shadow-2xl" alt="" />
                 </div>
             </div>
 
             <div class="hero">
                 <div class="hero-content flex-col lg:flex-row-reverse gap-10">
-                    <img src={confidetnAgro} class="max-w-lg w-full rounded-lg shadow-2xl" alt="" />
+                    <img src={"https://i.ibb.co/JmggjwH/confident-agro.jpg"} class="max-w-lg w-full rounded-lg shadow-2xl" alt="" />
                     <div>
                         <h1 class="text-5xl font-bold">Confident Agro, Dairy & Fisheries</h1>
                         <p class="py-6">
